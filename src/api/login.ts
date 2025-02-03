21d406b163190819cc9b69429049b97584275318
Kyrylo <kyrylo.ivanov17@outlook.com>
2025-02-03 19:03:04 +0200

    added some style changes

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,28 +1,3 @@
-// export const fetchLogin = () => {
-//     const url =
-//       "http://ec2-52-23-205-94.compute-1.amazonaws.com:3000/api/auth/login"; // API endpoint
-//     const data = {
-//       email: "user@example.com",
-//       password: "password123",
-//     }; 
-//     fetch(url, {
-//       method: "POST", 
-//       headers: {
-//         "Content-Type": "application/json", 
-//       },
-//       body: JSON.stringify(data),
-//     })
-//       .then((response) => response.json())
-//       .then((data) => {
-//         if (data?.access_token) {
-//           return data?.access_token
-//         }
-//       })
-//       .catch((error) => {
-//         console.error("Error:", error); 
-//       });
-//   };
-
   export const fetchLogin = async () => {
     const url =
     "http://ec2-52-23-205-94.compute-1.amazonaws.com:3000/api/auth/login"; // API endpoint
